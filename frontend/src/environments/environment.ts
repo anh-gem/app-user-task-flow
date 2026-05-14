cfb3aaeed1aca130e2ed985ae24c983ac83f409a
anh-gem <meghna.prakash.sarkar@gmail.com>
2026-05-14 19:29:22 +0530

    environment.ts updated

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:5000',
-  apiUrl: 'https://app-user-task-flow.onrender.com',
+  apiUrl: 'http://localhost:5000',
 };
